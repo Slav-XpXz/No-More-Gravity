2d1047989737416d4f3829a512175e9310cf7e9e
MilkManKarlson <76703277+MilkManKarlsonOnTop@users.noreply.github.com>
2022-07-08 13:25:30 -0400

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,12 +1,9 @@
-﻿namespace GorillaTagModTemplateProject
+namespace NoMoreGravity
 {
-	/// <summary>
-	/// This class is used to provide information about your mod to BepInEx.
-	/// </summary>
-	class PluginInfo
-	{
-		public const string GUID = "com.AuthorLowerName.gorillatag.GorillaTagLowerModTemplateProject";
-		public const string Name = "GorillaTagModTemplateProject";
-		public const string Version = "1.0.0";
-	}
+    class PluginInfo
+    {
+        public const string GUID = "slavXpXz.NoMoreGravity";
+        public const string Name = "NoMoreGravity";
+        public const string Version = "1.0.0";
+    }
 }
