c6a1af63f31c6dafc01bd0de2d766b9c60afc706
MilkManKarlson <76703277+MilkManKarlsonOnTop@users.noreply.github.com>
2022-07-08 13:24:55 -0400

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,85 +1,53 @@
 using BepInEx;
-using GorillaLocomotion;
-using System;
 using UnityEngine;
+using UnityEngine.XR;
 using Utilla;
+using System.ComponentModel;
 
-namespace GorillaTagModTemplateProject
+namespace NoMoreGravity
 {
-	/// <summary>
-	/// This is your mod's main class.
-	/// </summary>
+    [Description("HauntedModMenu")]
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool InModdedRoom;
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+            GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().useGravity = true;
+        }
+        void Update()
+        {
+            if (InModdedRoom)
+            {
+                bool NoGravity; InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.primaryButton, out NoGravity);
+                if (NoGravity)
+                {
+                    GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().useGravity = false;
+                }
+                else if (!NoGravity)
+                {
+                    GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().useGravity = true;
+                }
+            }
+        }
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            InModdedRoom = true;
+        }
 
-
-	[ModdedGamemode]
-	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
-	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-	public class Plugin : BaseUnityPlugin
-	{
-		bool inRoom;
-		private Rigidbody PlayerBody;
-
-		void OnEnable() {
-
-
-			HarmonyPatches.ApplyHarmonyPatches();
-			Utilla.Events.GameInitialized += OnGameInitialized;
-
-		}
-
-
-		void OnDisable() {
-
-
-			HarmonyPatches.RemoveHarmonyPatches();
-			Utilla.Events.GameInitialized -= OnGameInitialized;
-		}
-
-		void OnGameInitialized(object sender, EventArgs e)
-		{
-			PlayerBody = ((Collider)Player.Instance.bodyCollider).attachedRigidbody;
-		}
-
-		void Update()
-		{
-
-		}
-
-
-		void Main()
-		{
-			if (OVRInput.Get(OVRInput.Button.Four))
-			{
-				if (PlayerBody.useGravity == true)
-				{
-					PlayerBody.useGravity = false;
-				}
-				else
-				{
-					PlayerBody.useGravity = true;
-				}
-			}
-
-		}
-
-		[ModdedGamemodeJoin]
-		public void OnJoin(string gamemode)
-		{
-			Main();
-			inRoom = true;
-		}
-
-		[ModdedGamemodeLeave]
-		public void OnLeave(string gamemode)
-		{
-			/* Turn off statment */
-
-			if(PlayerBody.useGravity == false)
-            		{
-				PlayerBody.useGravity = true;
-			}
-
-			inRoom = false;
-		}
-	}
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().useGravity = true;
+            InModdedRoom = false;
+        }
+    }
 }
